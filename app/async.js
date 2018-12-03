0ba7eec1c99a5880ea7060ef6f802f5e451f7566
Andrii Vinohradov <avinohradov@sphereinc.com>
2018-12-03 20:34:02 +0200

    answers

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    const def = $.Deferred();
 
+    setTimeout(function(){
+      def.resolve(value);
+    }, 300);
+
+    return def.promise();
   },
 
   manipulateRemoteData: function(url) {
-
+    return $.ajax(url).then(function(res, status){
+      if (status === 'success') {
+        return res.people.map(item => item.name).sort();
+      }
+    });
   }
 };
--- app/count.js
+++ app/count.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    console.log(start);
+    this.interval = setInterval(() =>{
+      start = start + 1;
+      console.log(start);
+      if (start === end) clearInterval(this.interval);
+    }, 100);
 
+    return this;
+  },
+  cancel: function() {
+    clearInterval(this.interval);
   }
 };
